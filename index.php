434bcd13cfa8dddec3a6b51b2b9e97a0e2532758
Kilahcim <krzysztof.michalik01@gmail.com>
2018-02-26 16:28:33 +0100

    another psd to html project

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+     <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+     <link rel="stylesheet" href="css/style.css">
+     <link rel="stylesheet" href="css/breakpoints.css">
+     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
+     <script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <title>Bootsrap-3</title>
+  </head>
+  <body>
+    <div class="background">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">BOOTSTRAP TEMPLATE</a>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">BLOCK DEMOS 1 <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">BLOCK DEMOS 2 <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SITE TEMPLATES 1 <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SITE TEMPLATES 2 <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+              <li class="drop down box">
+                  FREE DOWNLOAD
+              </li>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+      <section class="header">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-md-8 col-md-offset-2">
+              <h1 class="text-padding">Free Bootstrap Agency Template</h1>
+              <p class="text-padding">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+
+            </div>
+              <div class="col-xs-12 col-md-6 ">
+              <div class="border-box right">
+                FREE DOWNLOAD
+              </div>
+            </div>
+              <br>
+              <div class="col-xs-12 col-md-6 ">
+              <div class="border-box left">
+                LIVE DEMO
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </div>
+    <section class="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-8">
+            <h1 class="text-padding text-center">Contact us</h1>
+            <p class="text-padding text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do ei usmod tempor incididunt Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nos</p>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <div class="text-center">
+              <form class="text-center" action="" method="post">
+                <input type="text" name="" value="" placeholder="Name">
+                <input type="text" name="" value="" placeholder="Email">
+                <input type="number" name="" value="" placeholder="Phone">
+                <input type="submit" name="" value="SEND FORM" placeholder="submit">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="socialmediamarketing">
+      <div class="container whitpadding">
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <div class="white">
+              <img src="images/backgroundsocial.jpg" alt="socialmedia-marketing">
+              <h1 class="text-padding text-center">Social media marketing</h1>
+              <p class="text-padding text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do ei usmod tempor incididunt Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nos</p>
+              <div class="box-black">
+                Learn More
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <div class="white">
+              <img src="images/backgroundsocial.jpg" alt="socialmedia-marketing">
+              <h1 class="text-padding text-center">Social media marketing</h1>
+              <p class="text-padding text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do ei usmod tempor incididunt Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nos</p>
+              <div class="box-black">
+                Learn More
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="offert">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <div class="white">
+              <h1>Join us</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco labori</p>
+              <div class="margin-leftright">
+                <h3>Interesting project</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco labori</p>
+                <h3>Free 2 mounth education</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco labori</p>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <div class="img">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="whatwedid">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <div class="text">
+              <h1 class="text-center">What we did</h1>
+              <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+            </div>
+
+            <div class="img">
+
+            </div>
+          </div>
+
+        <div class="col-xs-12 col-md-6">
+          <div class="row">
+            <div class="col-xs-12 col-md-6">
+              <h1>999</h1>
+              <h3 class="text-center">Happy clients</h3>
+              <p class="text-center ">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+            </div>
+            <div class="col-xs-12 col-md-6">
+              <h1>1999</h1>
+              <h3 class="text-center">Finished project</h3>
+              <p class="text-cente r">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+            </div>
+            <div class="col-xs-12 col-md-6">
+              <h1>17</h1>
+              <h3 class="text-center">Employees</h3>
+              <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+            </div>
+            <div class="col-xs-12 col-md-6">
+              <h1>2</h1>
+              <h3 class="text-center">Years</h3>
+              <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </section>
+    <section class="learn">
+      <div class="container">
+        <div class="col-xs-12 col-md-6">
+          <div class="right">
+            <div class="box-black">
+              <div class="eye-icon">
+                <i class="fas fa-eye"></i>
+              </div>
+               SEE MORE
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-md-6 left">
+          <div class="left">
+              <h1 class="text-center">Learn more about us</h1>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+    <section class="cutepets">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1 class="text-center">Ouer clients say:</h1>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <div class="doger">
+
+            </div>
+            <p class="text-center margin-leftright">In molestie justo et risus lobortis malesuada aliquet ac risus. Morbi dirginssim sagittis fringilla.</p>
+            <p class="text-center bold">Flaffy Smith </p>
+            <p class="text-center">Developer</p>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <div class="doger">
+
+            </div>
+            <p class="text-center margin-leftright">In molestie justo et risus lobortis malesuada aliquet ac risus. Morbi dirginssim sagittis fringilla.</p>
+            <p class="text-center bold">Flaffy Smith </p>
+            <p class="text-center">Developer</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="social">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1 class="text-center">Join us!</h1>
+            <ul class="text-center social-icon">
+              <li><i class="fab fa-twitter"></i></li>
+              <li><i class="fab fa-facebook"></i></li>
+              <li><i class="fab fa-instagram"></i></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <div class="col-xs-12 col-md-6">
+              <h2 class="text-center">Address</h2>
+              <p class="text-center">1234 Street Name</p>
+              <p class="text-center">City 999AE</p>
+            </div>
+            <div class="col-xs-12 col-md-6">
+              <h2 class="text-center">Contacts</h2>
+              <p class="text-center">Email office@gmail.com</p>
+              <p class="text-center">Phone 123456789</p>
+              <p class="text-center">Fax 123456789</p>
+            </div>
+            <div class="col-xs-12 col-md-6">
+              <h2 class="text-center">Links</h2>
+              <p class="text-center">Website builder</p>
+              <p class="text-center">Download for Windows</p>
+              <p class="text-center">Download for Mac</p>
+            </div>
+            <div class="col-xs-12 col-md-6">
+              <h2 class="text-center">Feedback</h2>
+              <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-md-6">
+            <div class="mymargin">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2550.610513822318!2d19.00941381603439!3d50.26185827944974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716ce3f13915cf7%3A0x4375cf1876aefd12!2sSobieskiego+11%2C+40-082+Katowice!5e0!3m2!1spl!2spl!4v1519382041417" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <p class="text-center last-one">&#9400; Copyrights 2017 Free Bootsrap Tamplates - All Rights Reserved</p>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <ul class="social-icon text-center">
+              <li><i class="fab fa-twitter"></i></li>
+              <li><i class="fab fa-facebook"></i></li>
+              <li><i class="fab fa-youtube"></i></li>
+              <li><i class="fab fa-instagram"></i></li>
+              <li><i class="fab fa-behance"></i></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+    </body>
+</html>
